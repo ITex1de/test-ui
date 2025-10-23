126fe1d149e7265e3f6059503b360c411a14f567
ITex1de <IT.T4nk>
2025-10-23 15:37:50 +0300

    0.2

--- main.cpp
+++ main.cpp
@@ -6,5 +6,9 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    
+    string a;
+    cout << "yes or no";
+    cin >> a;
+    cout << "lox!";
+    return 0;
 }
